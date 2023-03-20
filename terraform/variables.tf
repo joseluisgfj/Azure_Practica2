042810193f782e62a57b3efd49b1abc2787bf459
joseluisgfj <122015613+joseluisgfj@users.noreply.github.com>
2023-03-20 19:03:06 +0100

    Add files via upload

---
+++ terraform/variables.tf
@@ -0,0 +1,15 @@
+variable "resource_group_name" {
+  default = "jlgf-resource_group"
+}
+
+variable "location_name" {
+  default = "uksouth"
+}
+
+variable "network_name" {
+  default = "vnet1"
+}
+
+variable "subnet_name" {
+  default = "subnet1"
+}
